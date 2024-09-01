d5a6b8b9f8f9ae08ccd30a8903f7633af27b237c
Henrique Andrade <henriquenascandrade@outlook.com>
2024-09-01 13:40:03 -0300

    add limit slice jobs data

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -26,14 +26,13 @@ onMounted(() => { getJobs() })
             </h2>
 
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+                <JobListing v-for="job in jobs.slice(0, limit)" :key="job.id" :job="job" />
             </div>
         </div>
     </section>
 
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
       <a
-        href="jobs.html"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
         >Ver todas as vagas</a
       >
